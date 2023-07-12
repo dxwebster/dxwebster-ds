3bada19ed08a34a129cd114004981e9f7bbac520
dxwebster <adrianalimafm@gmail.com>
2023-07-12 20:36:47 -0300

    first commit

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,12 @@
+// re-exporta para na importaçao nao precisar repetir o nome do aquivo
+// ex: import {colors} from '@dxwebster-ds/tokens'
+
+export * from "./colors"
+
+export * from "./space"
+export * from "./radii"
+
+export * from "./fonts"
+export * from "./font-sizes"
+export * from "./font-weigths"
+export * from "./line-heights"
