b208471fb64282ed0866790dd13a66765746cedc
dxwebster <adrianalimafm@gmail.com>
2023-07-13 10:26:28 -0300

    add another tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -11,7 +11,7 @@ export * from "./line-heights"
 // re-exporta para na importaçao nao precisar repetir o nome do aquivo
 // ex: import {colors} from '@dxwebster-ds/tokens'
 
-// outros tokens
+// outros tokens que podem ser adicionados
 // opacity
 // box-shadow
 // animations
