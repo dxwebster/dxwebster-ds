c149a547f08f87a60c1153dbce41269a06930fa7
dxwebster <adrianalimafm@gmail.com>
2023-08-08 21:15:10 -0300

    change js to ts

--- packages/docs/src/stories/header/Header.tsx
+++ packages/docs/src/stories/header/Header.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-import { Button } from './Button';
-import './header.css';
+import { Button } from '../button'
+import './header.css'
 
-export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
+const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
     <div className="storybook-header">
       <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g fill="none" fillRule="evenodd">
             <path
               d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
@@ -37,13 +41,20 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         ) : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button
+              primary
+              size="small"
+              onClick={onCreateAccount}
+              label="Sign up"
+            />
           </>
         )}
       </div>
     </div>
   </header>
-);
+)
+
+export { Header }
 
 Header.propTypes = {
   user: PropTypes.shape({
@@ -52,8 +63,8 @@ Header.propTypes = {
   onLogin: PropTypes.func.isRequired,
   onLogout: PropTypes.func.isRequired,
   onCreateAccount: PropTypes.func.isRequired,
-};
+}
 
 Header.defaultProps = {
   user: null,
-};
+}
